fa84873d4a95106efb711acf5973127f71b1df6c
DebarunMitra <debarun96mitra@gmail.com>
2019-10-23 22:13:10 +0530

    app.js file created

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+Author: Debarun Mitra
+*/
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
