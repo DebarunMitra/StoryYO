e6724181e5574de665ae3368e900449271fb9fc2
DebarunMitra <debarun96mitra@gmail.com>
2019-10-23 22:14:28 +0530

    routes folder added

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
+// Load User Model
+require('./models/User');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+const stories = require('./routes/stories');
