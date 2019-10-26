0e931c84a986a636800fa374775d5e53bf8bcb68
Ranjith <ranju08134@gmail.com>
2019-10-26 15:21:51 +0530

    Updated

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+    mongoURI:'mongodb+srv://m001-student:m001-mongodb-basics@cluster0-w5qso.mongodb.net/tests',
+    googleClientID:'955500380140-eqlfv3prruuonu3gt9dvkjij8ivhuom6.apps.googleusercontent.com',
+    googleClientSecret:'Xccbd22IZDHsFRtjk0MGEDiL'
+}
