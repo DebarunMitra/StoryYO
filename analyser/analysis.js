6a1e50c2d3409b13cc5ba7ce736618d2f3503cda
DebarunMitra <debarun96mitra@gmail.com>
2019-10-29 07:47:18 +0530

    requirements added

--- analyser/analysis.js
+++ analyser/analysis.js
@@ -3,6 +3,10 @@ Author: Debarun Mitra
 Technlogy: MongoDB, ExpressJS, NodeJS
 Objective: Create an application where people can share their stories, ideas, thoughts and experiences.
 */
+const Gramma = require("gramma");
+const mongoose=require('mongoose');
+const User=mongoose.model('users');
+const Story=mongoose.model('stories');
 class Article{
  
 }
