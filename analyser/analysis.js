6e8ec89410e1718c85d7036a9a1c6ee7b9c12a1f
DebarunMitra <debarun96mitra@gmail.com>
2019-10-29 07:48:04 +0530

    article constructor created

--- analyser/analysis.js
+++ analyser/analysis.js
@@ -8,7 +8,17 @@ const mongoose=require('mongoose');
 const User=mongoose.model('users');
 const Story=mongoose.model('stories');
 class Article{
- 
+ constructor(storyId,storyBody,title,topic){
+    this.storyId=storyId;
+    this.storyBody=storyBody;
+    this.title=title;
+    this.topic=topic;
+    this.noWords=['is','are','am','have','had','was','were','be','been','can','could','shall','should','will','would'];
+    this.wordCount=0;
+    this.grammar=new Object();
+    this.sentenceCount=0;
+    this.words=new Object();
+  }
 }
 
 module.exports = Article;
