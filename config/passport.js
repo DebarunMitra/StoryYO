4d8160cd420529ec2d869934a29fbb5c0383792d
DebarunMitra <debarun96mitra@gmail.com>
2019-11-10 21:30:48 +0530

    config and helpers

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-const keys = require('./keys');
+const keys = require('./keys_dev');
 // Load user model
 const User = mongoose.model('users');
 
@@ -12,11 +12,6 @@ module.exports = function(passport){
       callbackURL:'/auth/google/callback',
       proxy: true
     }, (accessToken, refreshToken, profile, done) => {
-      // console.log(accessToken);
-      // console.log(profile);
-
-      //let image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
-      //console.log(image);
       const newUser = {
         googleID: profile.id,
         firstName: profile.name.givenName,
