710a6e60f03d4e71b3ad77f131f7b7542a1cb18c
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2019-11-14 15:22:31 +0530

    documented

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Story = mongoose.model('stories');
 const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
-
+//welcome page
 router.get('/', ensureGuest, (req, res) => {
   res.render('index/welcome');
 });
 
+//dashboard page after ensure authenticated
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
   Story.find({user:req.user.id})
   .then(stories => {
@@ -17,6 +18,7 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
   });
 });
 
+//about page
 router.get('/about', (req, res) => {
   res.render('index/about');
 });
