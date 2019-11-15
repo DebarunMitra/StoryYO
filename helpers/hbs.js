4d80842c9e8af0a91243fd1398ab0b6f0f8f3bae
DebarunMitra <debarun96mitra@gmail.com>
2019-11-15 11:50:50 +0530

    documenting and beautify done

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -1,4 +1,4 @@
-const moment=require('moment');
+const moment = require('moment');
 
 module.exports = {
   /**
@@ -7,22 +7,22 @@ module.exports = {
    * @param  {[number]} len [length of the article]
    * @return {[string]}     [send some part of article]
    */
-  truncate: function(str, len){
+  truncate: function(str, len) {
     if (str.length > len && str.length > 0) {
-			let new_str = str + " ";
-			new_str = str.substr(0, len);
-			new_str = str.substr(0, new_str.lastIndexOf(" "));
-			new_str = (new_str.length > 0) ? new_str : str.substr(0, len);
-			return new_str + '...';
-		}
-		return str;
+      let new_str = str + " ";
+      new_str = str.substr(0, len);
+      new_str = str.substr(0, new_str.lastIndexOf(" "));
+      new_str = (new_str.length > 0) ? new_str : str.substr(0, len);
+      return new_str + '...';
+    }
+    return str;
   },
   /**
    * [stripTags]
    * @param  {[string]} input [article string]
    * @return {[string]}       [remove html tag from article]
    */
-  stripTags: function(input){
+  stripTags: function(input) {
     return input.replace(/<(?:.|\n)*?>/gm, '');
   },
   /**
@@ -31,8 +31,8 @@ module.exports = {
    * @param  {[date format]} format [format of the date]
    * @return {[date wiht format]}        [date format to display into the title]
    */
-  formatDate: function(date, format){
-      return moment(date).format(format);
+  formatDate: function(date, format) {
+    return moment(date).format(format);
   },
   /**
    * [select(used in edit page)]
@@ -40,16 +40,16 @@ module.exports = {
    * @param  {[string]} options  [checks the option selected]
    * @return {[string]}          [return selected option]
    */
-  select: function(selected, options){
-    return options.fn(this).replace( new RegExp(' value=\"' + selected + '\"'), '$& selected="selected"').replace( new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
+  select: function(selected, options) {
+    return options.fn(this).replace(new RegExp(' value=\"' + selected + '\"'), '$& selected="selected"').replace(new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
   },
   /**
    * [senNo]
    * @param  {[string]} sen [takes an data sring with sentence no, word no, paragraph no and point]
    * @return {[type]}     [number of sentence]
    */
-  senNo:function(sen){
-    let sc=sen.split(':');
+  senNo: function(sen) {
+    let sc = sen.split(':');
     return sc[0];
   },
   /**
@@ -57,8 +57,8 @@ module.exports = {
    * @param  {[type]} word [takes an data sring with sentence no, word no, paragraph no]
    * @return {[type]}      [number of words]
    */
-  wordNo:function(word){
-    let wc=word.split(':');
+  wordNo: function(word) {
+    let wc = word.split(':');
     return wc[1];
   },
   /**
@@ -66,8 +66,8 @@ module.exports = {
    * @param  {[type]} para [takes an data sring with sentence no, word no, paragraph no]
    * @return {[type]}      [number of paragraphs]
    */
-  paragraphNo:function(para){
-    let pc=para.split(':');
+  paragraphNo: function(para) {
+    let pc = para.split(':');
     return pc[2];
   },
   /**
@@ -76,67 +76,81 @@ module.exports = {
    * @param  {[type]} options [description]
    * @return {[type]}         [description]
    */
-  eachProperty: function(value,options) {
-    let context=JSON.parse(value);
+  eachProperty: function(value, options) {
+    let context = JSON.parse(value);
     var ret = "";
     //console.log(typeof(context));
     //console.log(Object.keys(context).length);
-    for(let prop in context)
-    {
-        ret = ret + options.fn({property:prop,value:context[prop]});
+    for (let prop in context) {
+      ret = ret + options.fn({
+        property: prop,
+        value: context[prop]
+      });
     }
     return ret;
   },
   /**
    * [readingTime]
-   * @param  {[]} data [description]
-   * @return {[type]}      [description]
+   * @param  {[string]} data [takes the string]
+   * @return {[number]}      [reading time of article]
    */
-  readingTime:function(data){
-    let words=data.split(':');
-    let time=(words[1]/200)*60;
+  readingTime: function(data) {
+    let words = data.split(':');
+    let time = (words[1] / 200) * 60;
     return time.toFixed(2);
   },
-  speakingTime:function(data){
-    let words=data.split(':');
-    let time=(words[1]/130)*60;
+  /**
+   * [speakingTime]
+   * @param  {[string]} data [takes the string]
+   * @return {[number]}      [speaking time of article]
+   */
+  speakingTime: function(data) {
+    let words = data.split(':');
+    let time = (words[1] / 130) * 60;
     return time.toFixed(2);
   },
-  articlePoint:function(data){
-    let point=data.split(':');
+  /**
+   * [articlePoint]
+   * @param  {[string]} data [takes the string]
+   * @return {[number]}      [point of article]
+   */
+  articlePoint: function(data) {
+    let point = data.split(':');
     return point[3];
   },
   /**
    * [description]
    * @return {[string]} [image file path]
    */
-bgimage:function(){
-  return 'css/content.png';
+  bgimage: function() {
+    return 'css/content.png';
   },
   /**
    * [rank]
    * @param  {[array object]} stories [all stories of user]
    * @return {[number]}         [count all the points of article]
    */
-rank:function(stories){
-  let totalPoint=0,res=0;
-  stories.forEach((value,key)=>{
-    totalPoint+=value.point;res++;
-  });
-  res=totalPoint/res;
-  return res.toFixed(2);
-},
-/**
- * [editIcon]
- * @param  {[string]}  storyUser       [story user id]
- * @param  {[string]}  loggedUser      [logged user id]
- * @param  {[string]}  storyId         [story id]
- * @param  {Boolean} [floating=true] [icon status]
- * @return {[html tag]}              [show edit icon only for user story]
- */
-editIcon: function(storyUser, loggedUser, storyId, floating = true){
-    if(storyUser == loggedUser){
-      if(floating){
+  rank: function(stories) {
+    let totalPoint = 0,
+      res = 0;
+    stories.forEach((value, key) => {
+      totalPoint += value.point;
+      res++;
+    });
+    res = totalPoint / res;
+    return res.toFixed(2);
+  },
+  /**
+   * [editIcon]
+   * @param  {[string]}  storyUser       [story user id]
+   * @param  {[string]}  loggedUser      [logged user id]
+   * @param  {[string]}  storyId         [story id]
+   * @param  {Boolean} [floating=true] [icon status]
+   * @return {[html tag]}              [show edit icon only for user story]
+   */
+  editIcon: function(storyUser, loggedUser, storyId, floating = true) {
+    if (storyUser == loggedUser) {
+      if (floating) {
         return `<a href="/stories/edit/${storyId}" class="btn-floating halfway-fab red"><i class="fa fa-pencil"></i></a>`;
       } else {
         return `<a href="/stories/edit/${storyId}"><i class="fa fa-pencil"></i></a>`;
