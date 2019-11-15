f79b8647e569862caceb1b81a7d4689de2f0b0ed
DebarunMitra <debarun96mitra@gmail.com>
2019-11-15 09:43:31 +0530

    code beautified

--- routes/stories.js
+++ routes/stories.js
@@ -20,7 +20,9 @@ let promise;
 router.get('/', (req, res) => {
   Story.find({
     status: 'public'
-  }).populate('user').sort({date:'desc'}).then(stories => {
+  }).populate('user').sort({
+    date: 'desc'
+  }).then(stories => {
     res.render('stories/index', {
       stories: stories
     });
@@ -28,7 +30,7 @@ router.get('/', (req, res) => {
 });
 
 
-  /**
+/**
  * [Show Single Story]
  * @path '/show/:id'
  * @params story id
@@ -37,82 +39,87 @@ router.get('/', (req, res) => {
  */
 router.get('/show/:id', (req, res) => {
   Story.findOne({
-    _id: req.params.id
-  })
-  .populate('user')
-  .populate('comments.commentUser')
-  .then(story => {
-    if(story.status == 'public'){
-      res.render('stories/show', {
-        story:story
-      });
-    } else {
-      if(req.user){
-        if(req.user.id == story.user._id){
-          res.render('stories/show', {
-            story:story
-          });
+      _id: req.params.id
+    })
+    .populate('user')
+    .populate('comments.commentUser')
+    .then(story => {
+      if (story.status == 'public') {
+        res.render('stories/show', {
+          story: story
+        });
+      } else {
+        if (req.user) {
+          if (req.user.id == story.user._id) {
+            res.render('stories/show', {
+              story: story
+            });
+          } else {
+            res.redirect('/stories');
+          }
         } else {
           res.redirect('/stories');
         }
-      } else {
-        res.redirect('/stories');
       }
-    }
-  });
+    });
 });
 
 /**
-* [List stories from a user]
-* @path '/user/:userId'
-* @params user id
-* @method  GET
-* @res stories/index
-*/
+ * [List stories from a user]
+ * @path '/user/:userId'
+ * @params user id
+ * @method  GET
+ * @res stories/index
+ */
 router.get('/user/:userId', (req, res) => {
-  Story.find({user: req.params.userId, status: 'public'})
+  Story.find({
+      user: req.params.userId,
+      status: 'public'
+    })
     .populate('user')
     .then(stories => {
       res.render('stories/index', {
-        stories:stories
+        stories: stories
       });
     });
 });
 
 /**
-* [Logged in users stories]
-* @path '/my', user own story
-* @method  GET
-* @res stories/index
-*/
+ * [Logged in users stories]
+ * @path '/my', user own story
+ * @method  GET
+ * @res stories/index
+ */
 router.get('/my', ensureAuthenticated, (req, res) => {
-  Story.find({user: req.user.id})
+  Story.find({
+      user: req.user.id
+    })
     .populate('user')
     .then(stories => {
       res.render('stories/index', {
-        stories:stories
+        stories: stories
       });
     });
 });
 
 
 /**
-* [Add Story Form]
-* @path '/add', add story
-* @method  GET
-* @res stories/add
-*/
+ * [Add Story Form]
+ * @path '/add', add story
+ * @method  GET
+ * @res stories/add
+ */
 router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('stories/add');
 });
 
 /**
-* [rank of selected article]
-* @path '/rank/:id'
-* @params story id
-* @method  GET
-* @res 'stories/rank'
-*/
+ * [rank of selected article]
+ * @path '/rank/:id'
+ * @params story id
+ * @method  GET
+ * @res 'stories/rank'
+ */
 router.get('/rank/:id', (req, res) => {
   let sId = req.params.id;
   Story.findOne({
@@ -122,10 +129,10 @@ router.get('/rank/:id', (req, res) => {
     let wordSen = analysis.wordSentences();
     let newWord = analysis.newWord();
     let graSpell = analysis.grammerAndSpellCheck();
-    promise=graSpell;
+    promise = graSpell;
     res.render('stories/rank', {
       story: story,
-      graSpell:graSpell,
+      graSpell: graSpell,
       wordSen: wordSen,
       words: JSON.stringify(newWord)
     });
@@ -133,29 +140,29 @@ router.get('/rank/:id', (req, res) => {
 });
 
 /**
-* [show grammar and spelling mistakes of selected article]
-* @path '/graspell'
-* @method  GET
-* @res grammar and spelling mistakes
-*/
-router.get('/graspell',(req,res)=>{
-  let dataArr=[];
+ * [show grammar and spelling mistakes of selected article]
+ * @path '/graspell'
+ * @method  GET
+ * @res grammar and spelling mistakes
+ */
+router.get('/graspell', (req, res) => {
+  let dataArr = [];
   console.log(promise);
-   promise.then(value=>{
-     console.log(value);
-     res.send(value);
-   });
+  promise.then(value => {
+    console.log(value);
+    res.send(value);
+  });
 });
 
 
 /**
-* [save article points]
-* @path '/point/:id/:point'
-* @params story id
-* @params story point
-* @method  PUT
-* @res '/dashboard'
-*/
+ * [save article points]
+ * @path '/point/:id/:point'
+ * @params story id
+ * @params story point
+ * @method  PUT
+ * @res '/dashboard'
+ */
 router.put('/point/:id/:point', (req, res) => {
   Story.findOne({
     _id: req.params.id
@@ -170,19 +177,19 @@ router.put('/point/:id/:point', (req, res) => {
 
 
 /**
-* [edit stories]
-* @path '/edit/:id'
-* @params story id
-* @method  GET
-* @res 'stories/edit'
-*/
+ * [edit stories]
+ * @path '/edit/:id'
+ * @params story id
+ * @method  GET
+ * @res 'stories/edit'
+ */
 router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Story.findOne({
     _id: req.params.id
   }).then((story) => {
-    if(story.user!=req.user.id){
+    if (story.user != req.user.id) {
       res.redirect('/stories');
-    }else{
+    } else {
       res.render('stories/edit', {
         story: story
       });
@@ -192,11 +199,11 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
 
 
 /**
-* [process add Story]
-* @path '/', post default path to save story
-* @method  POST
-* @res ''/stories/show/storyId'
-*/
+ * [process add Story]
+ * @path '/', post default path to save story
+ * @method  POST
+ * @res ''/stories/show/storyId'
+ */
 router.post('/', (req, res) => {
   let allowComments;
   if (req.body.allowComments) {
@@ -220,12 +227,12 @@ router.post('/', (req, res) => {
 });
 
 /**
-* [edit form process]
-* @path '/:id'
-* @params story id
-* @method  PUT
-* @res '/dashboard'
-*/
+ * [edit form process]
+ * @path '/:id'
+ * @params story id
+ * @method  PUT
+ * @res '/dashboard'
+ */
 router.put('/:id', (req, res) => {
   Story.findOne({
     _id: req.params.id
@@ -249,12 +256,12 @@ router.put('/:id', (req, res) => {
 });
 
 /**
-* [delete story from db]
-* @path '/:id'
-* @params story id
-* @method  DELETE
-* @res '/dashboard'
-*/
+ * [delete story from db]
+ * @path '/:id'
+ * @params story id
+ * @method  DELETE
+ * @res '/dashboard'
+ */
 router.delete('/:id', (req, res) => {
   Story.remove({
     _id: req.params.id
@@ -264,30 +271,30 @@ router.delete('/:id', (req, res) => {
 });
 
 /**
-* [Add Comment]
-* @path '/comment/:id'
-* @params story id
-* @method  POST
-* @res '/stories/show/storyId'
-*/
+ * [Add Comment]
+ * @path '/comment/:id'
+ * @params story id
+ * @method  POST
+ * @res '/stories/show/storyId'
+ */
 router.post('/comment/:id', (req, res) => {
   Story.findOne({
-    _id: req.params.id
-  })
-  .then(story => {
-    const newComment = {
-      commentBody: req.body.commentBody,
-      commentUser: req.user.id
-    }
+      _id: req.params.id
+    })
+    .then(story => {
+      const newComment = {
+        commentBody: req.body.commentBody,
+        commentUser: req.user.id
+      }
 
-    // Add to comments array
-    story.comments.unshift(newComment);
+      // Add to comments array
+      story.comments.unshift(newComment);
 
-    story.save()
-      .then(story => {
-        res.redirect(`/stories/show/${story.id}`);
-      });
-  });
+      story.save()
+        .then(story => {
+          res.redirect(`/stories/show/${story.id}`);
+        });
+    });
 });
 
 module.exports = router;
