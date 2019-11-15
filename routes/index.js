780dd9a230b60a2998b0cb78cfc769a16e0376dc
DebarunMitra <debarun96mitra@gmail.com>
2019-11-15 09:51:42 +0530

    index.js documenting

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,22 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Story = mongoose.model('stories');
 const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
-//welcome page
+/**
+ * [welcome page]
+ * @path '/', index js default path
+ * @method  GET
+ * @res 'index/welcome'
+ */
 router.get('/', ensureGuest, (req, res) => {
   res.render('index/welcome');
 });
 
-//dashboard page after ensure authenticated
+/**
+ * [dashboard page after ensure authenticated]
+ * @path '/dashboard'
+ * @method  GET
+ * @res 'index/dashboard'
+ */
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
   Story.find({user:req.user.id})
   .then(stories => {
@@ -18,7 +28,12 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
   });
 });
 
-//about page
+/**
+ * [about page]
+ * @path '/about'
+ * @method  GET
+ * @res 'index/about'
+ */
 router.get('/about', (req, res) => {
   res.render('index/about');
 });
